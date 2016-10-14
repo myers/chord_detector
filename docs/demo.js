057a42d092d14ca8e19f2b39fdeb30640d4ca72d
Myers Carpenter <myers@teamtreehouse.com>
2016-10-14 00:55:18 -0400

    better demo

--- docs/demo.js
+++ docs/demo.js
@@ -5,7 +5,7 @@ $(function() {
 
   var chromagram
   var source
-  var currentChroma;
+  var currentChroma = new Float32Array(12)
   const chordDetector = new cd.ChordDetector()
   var output
 
@@ -17,7 +17,7 @@ $(function() {
       el.currentTime = 0
     })
 
-    output = $(this).parent().find(".chord")
+    output = $('#chord-detection')
 
     if (chromagram) {
       chromagram._free()
@@ -26,6 +26,7 @@ $(function() {
 
     source = audioCtx.createMediaElementSource(this)
     $(this).on('ended', () => {
+      currentChroma.fill(0)
       source.disconnect(scriptNode)
       scriptNode.disconnect(audioCtx.destination)
       this.currentTime = 0
@@ -34,20 +35,46 @@ $(function() {
     scriptNode.connect(audioCtx.destination)
   })
 
+
+  $('#chroma-visualizer').html('<canvas width="800" height="256">');
+  visualizationCtx = $('#chroma-visualizer canvas').get(0).getContext('2d');
+  const gradient = visualizationCtx.createLinearGradient(0, 0, 0, 512);
+  gradient.addColorStop(1, '#000000');
+  gradient.addColorStop(0.75, '#2ecc71');
+  gradient.addColorStop(0.25, '#f1c40f');
+  gradient.addColorStop(0, '#e74c3c');
+
+  notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+  function updateVisualization() {
+    visualizationCtx.clearRect(0, 0, 800, 256);
+
+    for (let i = 0; i < currentChroma.length; i++) {
+      const value = currentChroma[i];
+      visualizationCtx.fillStyle = gradient;
+      visualizationCtx.fillRect(i * 65, 246, 10, value * -7);
+
+      visualizationCtx.fillStyle = 'black';
+      visualizationCtx.fillText(notes[i], i * 65, 256);
+    }
+
+    requestAnimationFrame(updateVisualization);
+  }
+  updateVisualization();
+
   const scriptNode = audioCtx.createScriptProcessor(1024, 1, 1)
   scriptNode.onaudioprocess = function(audioProcessingEvent) {
     var inputBuffer = audioProcessingEvent.inputBuffer;
     var outputBuffer = audioProcessingEvent.outputBuffer;
+    // Loop through the output channels (in this case there is only one)
+    for (var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
+      outputBuffer.copyToChannel(inputBuffer.getChannelData(channel), channel)
+    }
+
     chromagram.processAudioFrame(inputBuffer)
     if (chromagram.isReady()) {
       currentChroma = chromagram.getChromagram()
       chordDetector.detectChord(currentChroma)
-      output.html("Detected " + chordDetector.rootNote() + " " + chordDetector.quality() + " " +  chordDetector.intervals())
-    }
-
-    // Loop through the output channels (in this case there is only one)
-    for (var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
-      outputBuffer.copyToChannel(inputBuffer.getChannelData(channel), channel)
+      output.html("Is it " + chordDetector.rootNote() + " " + chordDetector.quality() + " " +  chordDetector.intervals() + "?")
     }
   }
 })
